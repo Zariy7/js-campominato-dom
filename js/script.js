5413f363d05536896ed9d97df301410e6d638a16
Zariy7 <nazariy.hereha@gmail.com>
2023-11-30 16:33:32 +0100

    both bonuses completed

--- js/script.js
+++ js/script.js
@@ -41,12 +41,17 @@ function gridGen(){
             let z = ((i+1)*diffValue)-(diffValue-(j+1));
             newCol.textContent = z;
 
+            if(bombsArray.includes(z)){
+                newCol.classList.add("hidden-bomb");
+            }
+
             bombSort(bombsArray, z, newCol);
             newRow.appendChild(newCol);
         }
     }
 }
 
+//GENERATES AN ARRAY OF 16 ELEMENTS WITH NO REPETITIONS WITHIN ITSELF.
 function bombGen (valueMax){
     let array = [];
     let randomNumber;
@@ -64,17 +69,37 @@ function bombGen (valueMax){
     return array;
 }
 
-function bombSort (array, index, square){
-    if(array.includes(index)){
-        square.addEventListener('click', function(){
-            square.classList.toggle("danger");
-            console.log('BOOM!');
-        })
+//USER CLICKS ON BOMB
+function bombClick (){
+    //ALL BOMBS GET REVEALED UPON CLICKING
+    let allBombs = document.getElementsByClassName('hidden-bomb');
+    for(let i = 0; i<allBombs.length; i++){
+        allBombs[i].classList.toggle("basic");
+        allBombs[i].classList.toggle("danger");
     }
-    else{        
-        square.addEventListener('click', function(){
-            square.classList.toggle("safe");
-            console.log('No boom.');
-        })
+    
+    //SQUARES ARE NOT CLICKABLE ANYMORE UPON CLICKING A BOMB
+    let allSquares = document.getElementsByClassName('standard-square');
+    for(let i = 0; i<allSquares.length; i++){
+        allSquares[i].removeEventListener('click', bombClick);
+        allSquares[i].removeEventListener('click', safeClick);
+    }
+}
+
+//USER CLICKS ON SAFE SPOT
+function safeClick (){
+    this.classList.toggle("safe");
+    this.classList.toggle("basic");
+}
+
+//DISTINGUISHES CLICKING ON A BOMB FROM CLICKING ON A SAFE SPOT.
+function bombSort (array, index, square){
+    //USER CLICKS ON BOMB
+    if(array.includes(index)){
+        square.addEventListener('click', bombClick);
+    }
+    //USER CLICKS ON SAFE SQUARE
+    else{
+        square.addEventListener('click', safeClick);
     }
 }
